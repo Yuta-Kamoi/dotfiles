c9debb0f1022e027c600423dfe8bf5b52e633e62
Yuta-Kamoi <92435802+Yuta-Kamoi@users.noreply.github.com>
2026-05-14 20:45:40 +0900

    update .zshrc

--- zsh/.zprofile.d/wsl.zsh
+++ zsh/.zprofile.d/wsl.zsh
@@ -4,13 +4,13 @@ path=(/usr/local/{bin,sbin} $path)
 # Linuxbrewのパスを、現在のPATHのさらに先頭（最優先）に追加
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 
-# # Google Drive (G:)を起動時に自動マウントする
-# # 既にマウントされていなければ実行
-# if ! mountpoint -q /mnt/g; then
-#   echo "G: ドライブをマウントします..."
-#   # 既存の /mnt/g が存在しない場合作成（通常は存在しますが念のため）
-#   [ ! -d /mnt/g ] && sudo mkdir -p /mnt/g
+# Google Drive (G:)を起動時に自動マウントする
+# 既にマウントされていなければ実行
+if ! mountpoint -q /mnt/g; then
+  echo "G: ドライブをマウントします..."
+  # 既存の /mnt/g が存在しない場合作成（通常は存在しますが念のため）
+  [ ! -d /mnt/g ] && sudo mkdir -p /mnt/g
   
-#   # 手動で成功したコマンドを実行
-#   sudo mount -t drvfs G: /mnt/g
-# fi
+  # 手動で成功したコマンドを実行
+  sudo mount -t drvfs G: /mnt/g
+fi
